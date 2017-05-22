d87c35e079debb04c377858bad7d0ae7a6d60af0
Zifa Mathebula <zifamathebula@gmail.com>
2017-05-22 08:57:27 +0200

    Created base for page generation service

--- Wifi-Info-Client-App/src/main/java/za/co/wifi/info/client/events/AdvertSyncEvent.java
+++ Wifi-Info-Client-App/src/main/java/za/co/wifi/info/client/events/AdvertSyncEvent.java
@@ -1,5 +1,6 @@
 package za.co.wifi.info.client.events;
 
+import java.util.LinkedList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import za.co.wifi.info.client.service.AdvertService;
 import za.co.wifi.info.client.service.AdvertSyncService;
+import za.co.wifi.info.client.service.PageGeneratorService;
 import za.co.wifi.info.client.web.model.BannerLink;
 import za.co.wifi.info.client.web.model.Category;
 
@@ -17,28 +19,31 @@ public class AdvertSyncEvent extends AbstractAdvertEvent {
 
     private final AdvertService advertService;
     private final AdvertSyncService advertSyncService;
+    private final PageGeneratorService pageGeneratorService;
 
     @Autowired
     public AdvertSyncEvent(AdvertService advertService,
-            AdvertSyncService advertSyncService) {
+                           AdvertSyncService advertSyncService,
+                           PageGeneratorService pageGeneratorService) {
         this.advertService = advertService;
         this.advertSyncService = advertSyncService;
+        this.pageGeneratorService = pageGeneratorService;
     }
 
     @Scheduled(fixedRate = SYNC_INTERVAL)
     public void syncAdverts() {
-        
+
         advertSyncService.syncDevice();
-                     
+
         try {
             //Pause for 30 seconds
             Thread.sleep(30000L);
         } catch (InterruptedException ex) {
             LOGGER.error(ex.getMessage());
         }
-        
-        syncDeviceBannerLinks();
-        
+
+        List<BannerLink> bannerLinkAdverts = syncDeviceBannerLinks();
+
         LOGGER.info("Shuffling category adverts");
 
         try {
@@ -46,24 +51,30 @@ public class AdvertSyncEvent extends AbstractAdvertEvent {
             if (CollectionUtils.isEmpty(categoryAdverts)) {
                 categoryAdverts = advertService.generateCategoryAdverts();
             } else {
-                categoryAdverts = mergeAdverts(categoryAdverts,advertService.generateCategoryAdverts());
+                categoryAdverts = mergeAdverts(categoryAdverts, advertService.generateCategoryAdverts());
                 shuffleCategoryAdverts(categoryAdverts);
             }
 
             advertService.setCategoryAdverts(categoryAdverts);
+
+            pageGeneratorService.generatePage(bannerLinkAdverts,categoryAdverts);
         } catch (Exception ex) {
             LOGGER.error("Error shuffling Adverts");
         }
     }
 
-    public void syncDeviceBannerLinks() {
+    public List<BannerLink> syncDeviceBannerLinks() {
         LOGGER.info("Syncing banner links");
 
         try {
             List<BannerLink> bannerLinkAdverts = advertService.generateBannerLinkAdverts();
             advertService.setBannerLinkAdverts(bannerLinkAdverts);
+            
+            return bannerLinkAdverts;
         } catch (Exception ex) {
             LOGGER.error("Error syncing banner links");
         }
+
+        return new LinkedList<BannerLink>();
     }
 }
--- Wifi-Info-Client-App/src/main/java/za/co/wifi/info/client/service/PageGeneratorService.java
+++ Wifi-Info-Client-App/src/main/java/za/co/wifi/info/client/service/PageGeneratorService.java
@@ -1,13 +1,19 @@
 
 package za.co.wifi.info.client.service;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import za.co.wifi.info.client.web.model.BannerLink;
+import za.co.wifi.info.client.web.model.Category;
 
 @Service
 @Scope("singleton")
 public class PageGeneratorService {
     private static final Logger LOGGER = LoggerFactory.getLogger(PageGeneratorService.class.getName());
+
+    public void generatePage(List<BannerLink> bannerLinkAdverts, List<Category> categoryAdverts) {
+    }
 }
